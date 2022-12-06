c0ca6a87fbb86f0fa61f29db0b5bfabee27a2031
devShakib <devshakib015@gmail.com>
2022-12-06 14:01:33 +0600

    OTP page

--- customer_app/lib/main.dart
+++ customer_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:customer_app/helpers/config_loading.dart';
 import 'package:customer_app/helpers/hive_constants.dart';
 import 'package:customer_app/helpers/theme_constants.dart';
 import 'package:customer_app/providers/hive_provider.dart';
+import 'package:customer_app/views/auth/login_page.dart';
 import 'package:customer_app/views/wrapper.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -60,7 +61,7 @@ class MyApp extends ConsumerWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      home: const Wrapper(),
+      home: const LoginPage(),
       builder: EasyLoading.init(),
     );
   }
--- customer_app/lib/views/auth/sign_in_with_phone_page.dart
+++ customer_app/lib/views/auth/sign_in_with_phone_page.dart
@@ -1,6 +1,7 @@
 import 'package:customer_app/helpers/app_constants.dart';
 import 'package:customer_app/helpers/theme_constants.dart';
 import 'package:customer_app/views/auth/login_page.dart';
+import 'package:customer_app/views/auth/otp_verification_page.dart';
 import 'package:customer_app/widgets/custom_spacer.dart';
 import 'package:customer_app/widgets/custom_widgets.dart';
 import 'package:customer_app/widgets/field_card.dart';
@@ -20,100 +21,112 @@ class _SignInWithPhonePageState extends State<SignInWithPhonePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(ThemeConstant.defaultPadding * 2),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: ThemeConstant.defaultPadding),
-              const Hero(tag: "logo", child: StaticLogoWidget()),
-              const SizedBox(height: ThemeConstant.defaultPadding * 2),
-              Text(
-                "Sign in with phone number",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline6!.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
-              const SizedBox(height: ThemeConstant.defaultPadding * 3),
-              FieldCard(
-                child: InternationalPhoneNumberInput(
-                  countries: AppConstants.appCountryCodes,
-                  onInputChanged: (PhoneNumber number) {},
-                  ignoreBlank: false,
-                  autoValidateMode: AutovalidateMode.onUserInteraction,
-                  textFieldController: _phoneController,
-                  validator: (value) {
-                    return null;
-                  },
-                  spaceBetweenSelectorAndTextField: 0,
-                  formatInput: true,
-                  textAlignVertical: TextAlignVertical.top,
-                  inputDecoration: const InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.zero,
-                    hintText: "1234567890",
-                  ),
-                  keyboardType: const TextInputType.numberWithOptions(
-                      signed: true, decimal: true),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(ThemeConstant.defaultPadding * 2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: ThemeConstant.defaultPadding),
+                const Hero(tag: "logo", child: StaticLogoWidget()),
+                const SizedBox(height: ThemeConstant.defaultPadding * 2),
+                Text(
+                  "Sign in with phone number",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
                 ),
-              ),
-              const DefaultVerticalSpacer(),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Sign in"),
-              ),
-              const SizedBox(height: ThemeConstant.defaultPadding * 2),
-              Row(
-                children: [
-                  const Expanded(child: Divider()),
-                  const SizedBox(width: ThemeConstant.defaultPadding),
-                  Text(
-                    "or continue with",
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                  const SizedBox(width: ThemeConstant.defaultPadding),
-                  const Expanded(child: Divider()),
-                ],
-              ),
-              const SizedBox(height: ThemeConstant.defaultPadding * 2),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: Hero(
-                      tag: "apple",
-                      child: Icon(Ionicons.logo_apple,
-                          color: Theme.of(context).textTheme.subtitle2!.color),
+                const SizedBox(height: ThemeConstant.defaultPadding * 3),
+                FieldCard(
+                  child: InternationalPhoneNumberInput(
+                    countries: AppConstants.appCountryCodes,
+                    onInputChanged: (PhoneNumber number) {},
+                    ignoreBlank: false,
+                    autoValidateMode: AutovalidateMode.onUserInteraction,
+                    textFieldController: _phoneController,
+                    validator: (value) {
+                      return null;
+                    },
+                    spaceBetweenSelectorAndTextField: 0,
+                    formatInput: true,
+                    textAlignVertical: TextAlignVertical.top,
+                    inputDecoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.zero,
+                      hintText: "1234-567890",
                     ),
+                    keyboardType: TextInputType.number,
                   ),
-                  const DefaultHorizontalSpacer(),
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: const Hero(
-                      tag: "google",
-                      child: Icon(Ionicons.logo_google, color: Colors.red),
+                ),
+                const DefaultVerticalSpacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const OtpVerificationPage(),
+                      ),
+                    );
+                  },
+                  child: const Text("Sign in"),
+                ),
+                const SizedBox(height: ThemeConstant.defaultPadding * 2),
+                Row(
+                  children: [
+                    const Expanded(child: Divider()),
+                    const SizedBox(width: ThemeConstant.defaultPadding),
+                    Text(
+                      "or continue with",
+                      style: Theme.of(context).textTheme.subtitle2,
                     ),
-                  ),
-                  const DefaultHorizontalSpacer(),
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: const Hero(
-                      tag: "facebook",
-                      child: Icon(Ionicons.logo_facebook, color: Colors.blue),
+                    const SizedBox(width: ThemeConstant.defaultPadding),
+                    const Expanded(child: Divider()),
+                  ],
+                ),
+                const SizedBox(height: ThemeConstant.defaultPadding * 2),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Hero(
+                        tag: "apple",
+                        child: Icon(Ionicons.logo_apple,
+                            color:
+                                Theme.of(context).textTheme.subtitle2!.color),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: ThemeConstant.defaultPadding),
-              const Hero(tag: "terms", child: AgreeTermsAndPrivacySection()),
-              const DefaultVerticalSpacer(),
-            ],
+                    const DefaultHorizontalSpacer(),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: const Hero(
+                        tag: "google",
+                        child: Icon(Ionicons.logo_google, color: Colors.red),
+                      ),
+                    ),
+                    const DefaultHorizontalSpacer(),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: const Hero(
+                        tag: "facebook",
+                        child: Icon(Ionicons.logo_facebook, color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: ThemeConstant.defaultPadding),
+                const Hero(tag: "terms", child: AgreeTermsAndPrivacySection()),
+                const DefaultVerticalSpacer(),
+              ],
+            ),
           ),
         ),
       ),
